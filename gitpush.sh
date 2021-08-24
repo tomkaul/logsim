a6b84500396fa077001eab447c466ceabafe7aa1
Thomas Kaulberg (TomKaul) <thomas.kaulberg@gmail.com>
2021-08-24 19:59:12 +0200

    Added conda lists

--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Pushing to github with comment:" \"$@\"
-git add ./*.py ./*.sh ./logsim/*.py ./*.ipynb
+git add ./*.py ./*.sh ./logsim/*.py ./*.ipynb ./*.txt
 
 MSG="Update"
 if [ $# -gt 0 ]
